0427538b6daf1811f3a211c7e8900d216210a924
Ryan Owens <rtowens11@gmail.com>
2021-03-13 00:13:34 -0600

    built full grid with js

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,22 @@
 var dayContainer = document.getElementById('day')
+// var hourRow = document.createElement('div')
+// var rowItemHour = document.createElement('div')
+// var rowItemEvent = document.createElement('div')
+// var rowItemButton = document.createElement('div')
+
+var currentTime = moment()
+var workingHours = [1,2,3,4,5,6,7,8,9]
+
+for (var i = 0; i < workingHours.length; i++) {
+
 var hourRow = document.createElement('div')
 var rowItemHour = document.createElement('div')
 var rowItemEvent = document.createElement('div')
 var rowItemButton = document.createElement('div')
-
-var currentTime = 
-
+    
 rowItemHour.textContent = 'test rowItemHour'
 rowItemEvent.textContent = 'test rowItemEvent'
 rowItemButton.textContent = 'test rowItemButton'
-
-
 
 dayContainer.appendChild(hourRow)
 hourRow.appendChild(rowItemHour)
@@ -18,7 +24,9 @@ hourRow.appendChild(rowItemEvent)
 hourRow.appendChild(rowItemButton)
 
 hourRow.setAttribute('class', 'row')
+hourRow.setAttribute('id', ('hour'+(i+9)))
 
 rowItemHour.setAttribute('class', 'col-sm-2 bg-white border-top border-bottom border-dark')
 rowItemEvent.setAttribute('class', 'col-sm-8 bg-secondary border border-dark')
 rowItemButton.setAttribute('class', 'col-sm-2 bg-info border-top border-bottom border-dark rounded rounded-3')
+}
