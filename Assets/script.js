d6c09edd780f643a936efb98ed36a7e60f8566bb
Ryan Owens <rtowens11@gmail.com>
2021-03-13 15:14:40 -0600

    removed extra bootstrap to use existing css

--- Assets/script.js
+++ Assets/script.js
@@ -5,28 +5,32 @@ var dayContainer = document.getElementById('day')
 // var rowItemButton = document.createElement('div')
 
 var currentTime = moment()
-var workingHours = [1,2,3,4,5,6,7,8,9]
+var workingHours = [9,10,11,12,13,14,15,16,17]
 
-for (var i = 0; i < workingHours.length; i++) {
+for (var i = 9; i < (workingHours[8]+1); i++) {
 
 var hourRow = document.createElement('div')
 var rowItemHour = document.createElement('div')
 var rowItemEvent = document.createElement('div')
-var rowItemButton = document.createElement('div')
+var rowItemButton = document.createElement('button')
+var rowItemButtonIcon = document.createElement('i')
     
 rowItemHour.textContent = 'test rowItemHour'
 rowItemEvent.textContent = 'test rowItemEvent'
-rowItemButton.textContent = 'test rowItemButton'
+
 
 dayContainer.appendChild(hourRow)
 hourRow.appendChild(rowItemHour)
 hourRow.appendChild(rowItemEvent)
 hourRow.appendChild(rowItemButton)
+rowItemButton.appendChild(rowItemButtonIcon)
 
-hourRow.setAttribute('class', 'row')
+hourRow.setAttribute('class', 'row time-block')
 hourRow.setAttribute('id', ('hour'+(i+9)))
 
-rowItemHour.setAttribute('class', 'col-sm-2 bg-white border-top border-bottom border-dark')
-rowItemEvent.setAttribute('class', 'col-sm-8 bg-secondary border border-dark')
-rowItemButton.setAttribute('class', 'col-sm-2 bg-info border-top border-bottom border-dark rounded rounded-3')
+rowItemHour.setAttribute('class', 'col-sm-2 hour')
+rowItemEvent.setAttribute('class', 'col-sm-8 textarea past')
+rowItemButton.setAttribute('class', 'col-sm-2 saveBtn')
+
+rowItemButtonIcon.setAttribute('class', 'fas fa-save fa-2x')
 }
