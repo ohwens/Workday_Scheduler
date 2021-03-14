64f507e1c10ebe0cc5eb410698348261eef2a75a
Ryan Owens <rtowens11@gmail.com>
2021-03-13 23:35:18 -0600

    testing localStorage

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,5 @@
-var dayContainer = document.getElementById('day')
-// var hourRow = document.createElement('div')
-// var rowItemHour = document.createElement('div')
-// var rowItemEvent = document.createElement('div')
-// var rowItemButton = document.createElement('div')
+var dayContainer = document.getElementById('day');
+var saveButtons = $('#button');
 
 var currentHour = moment().hour()
 var workingHours = [9,10,11,12,13,14,15,16,17]
@@ -10,12 +7,9 @@ var workingHours = [9,10,11,12,13,14,15,16,17]
 for (var i = 9; i < workingHours[8] + 1; i++) {
     var hourRow = document.createElement("div");
     var rowItemHour = document.createElement("div");
-    var rowItemEvent = document.createElement("div");
+    var rowItemEvent = document.createElement("input");
     var rowItemButton = document.createElement("button");
     var rowItemButtonIcon = document.createElement("i");
-
-    // rowItemHour.textContent = 'test rowItemHour'
-    rowItemEvent.textContent = "test rowItemEvent";
 
     if (i < 13) {
         rowItemHour.textContent = i + "AM";
@@ -29,21 +23,48 @@ for (var i = 9; i < workingHours[8] + 1; i++) {
     hourRow.appendChild(rowItemButton);
     rowItemButton.appendChild(rowItemButtonIcon);
 
+
     hourRow.setAttribute("class", "row time-block");
-    hourRow.setAttribute("id", "hour" + (i + 9));
+    hourRow.setAttribute("id", "hour" + i);
 
     rowItemHour.setAttribute("class", "col-sm-2 hour ");
-    rowItemEvent.setAttribute("class", "col-sm-8 textarea past present future");
+    rowItemEvent.setAttribute("id", "event" + i);
     rowItemButton.setAttribute("class", "col-sm-2 saveBtn");
+    rowItemButton.setAttribute('id', 'button' + i)
 
     rowItemButtonIcon.setAttribute("class", "fas fa-save fa-2x");
 
     if (i === currentHour) {
-        rowItemEvent.setAttribute("class", "col-sm-8 textarea present");
+        rowItemEvent.setAttribute("class", "col-sm-8 textarea present form");
     } else if (i > currentHour) {
-        rowItemEvent.setAttribute("class", "col-sm-8 textarea future");
+        rowItemEvent.setAttribute("class", "col-sm-8 textarea future form");
     } else {
-        rowItemEvent.setAttribute("class", "col-sm-8 textarea past");
+        rowItemEvent.setAttribute("class", "col-sm-8 textarea past form");
     }
+
+    var eventNumber = 'event9' + i;
+    // var eventLocal = localStorage.eventNumber;
+    rowItemEvent.setAttribute('value', localStorage.eventNumber);
 }
 
+// for (let i = 0; i < workingHours[8] + 1; i++) {
+//     var eventNumber = 'event' + i;
+//     var eventLocal = localStorage.eventNumber;
+//     rowItemEvent.setAttribute('value', eventLocal);
+// }
+
+// 
+    
+// }
+// var userInput = document.getElementById('event9').value
+
+// function setLocalStorage(){
+
+// }
+
+
+// button17.addEventListener('click', function(event) {
+//     console.log(event)
+//     localStorage.setItem('event17', event17.value)
+
+// 
